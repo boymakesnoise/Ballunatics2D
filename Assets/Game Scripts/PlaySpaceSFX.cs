2692c1e1efe6ed6a877bd3e43dbc57e7856f1860
Magnus <magnus.thore@gmail.com>
2019-10-15 20:17:19 +0200

    lower gravity for players in space + put space hitbox on "ignore raycast" layer

--- Assets/Game Scripts/PlaySpaceSFX.cs
+++ Assets/Game Scripts/PlaySpaceSFX.cs
@@ -2,16 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlaySpaceSFX : MonoBehaviour
-{
+public class PlaySpaceSFX : MonoBehaviour {
+
+    public float spaceGravity = 0.5f;
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject player3;
+    public GameObject player4;
+
+    private bool canPlaySpaceSounds = true;
+
     void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.name == "Player1"
         || other.gameObject.name == "Player2"
         || other.gameObject.name == "Player3"
         || other.gameObject.name == "Player4") {
-            FindObjectOfType<AudioManager>().Stop("AmbienceWind");
-            FindObjectOfType<AudioManager>().Play("AmbienceSpace");
-            Destroy(gameObject);
+            if (canPlaySpaceSounds) {
+                FindObjectOfType<AudioManager>().Stop("AmbienceWind");
+                FindObjectOfType<AudioManager>().Play("AmbienceSpace");
+                canPlaySpaceSounds = false;     // start only once
+            }
+
+            //// Minska gravity
+            //player1.GetComponent<Rigidbody2D>().gravityScale = spaceGravity;
+            //player2.GetComponent<Rigidbody2D>().gravityScale = spaceGravity;
+            //player3.GetComponent<Rigidbody2D>().gravityScale = spaceGravity;
+            //player4.GetComponent<Rigidbody2D>().gravityScale = spaceGravity;
+
+            //Destroy(gameObject);
         }
+
+        if (other.gameObject.name == "Player1")
+            player1.GetComponent<Rigidbody2D>().gravityScale = spaceGravity;
+        if (other.gameObject.name == "Player2")
+            player2.GetComponent<Rigidbody2D>().gravityScale = spaceGravity;
+        if (other.gameObject.name == "Player3")
+            player3.GetComponent<Rigidbody2D>().gravityScale = spaceGravity;
+        if (other.gameObject.name == "Player4")
+            player4.GetComponent<Rigidbody2D>().gravityScale = spaceGravity;
     }
 }
