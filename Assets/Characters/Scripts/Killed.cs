0b1369dbcc74ad12db7596562add0d0c221ba2fe
Magnus <magnus.thore@gmail.com>
2019-10-15 21:35:09 +0200

    BLOOD SPLATTER when players DIIIIE!!!!!!

--- Assets/Characters/Scripts/Killed.cs
+++ Assets/Characters/Scripts/Killed.cs
@@ -6,6 +6,7 @@ public class Killed : MonoBehaviour
 {
     public GameObject pil;
     public CountRemainingPlayers deathController;
+    public GameObject blood;
 
     private void Start() {
         deathController.playerHasAppeared();
@@ -18,6 +19,9 @@ public class Killed : MonoBehaviour
             deathController.playerHasDied();
 
             FindObjectOfType<AudioManager>().Play("PlayerDeath");
+
+            // Spawn blood-particle-system
+            Instantiate(blood, transform.position, transform.rotation);
 
             Destroy(pil);
             Destroy(this.gameObject);
